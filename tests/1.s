ca43a68e3cac6c4f6fb2fb1912ef64b861bcf945
salvor <yinmingzhuo@gmail.com>
2023-04-25 18:11:36 +0800

    choro

--- tests/1.s
+++ tests/1.s
@@ -1,23 +1,12 @@
 .globl main
-.data
-test_str: .string "Hello, world!"
-test_word:
-    .word 1 2 3 4
-test_byte:
-    .byte 1 2 3 4 5
-test_half:
-    .half 1 2 3 4 5
 .text
-    add x0, x0, x0
-    add x0, x0, x0
 main:
-    lw x1, 0x10(x0)
-    addi x1, x1, 1
-    addi x2, x1, 1
-    addi x3, x2, 1
-    addi x4, x3, 1
-    addi x5, x4, 1
-    addi x6, x5, 1
-    addi x7, x6, 1
+    addi x1, x0, 1
+    addi x2, x0, 1
+    addi x3, x0, 1
+    addi x4, x0, 1
+    addi x5, x0, 1
+    addi x6, x0, 1
+    addi x7, x0, 1
     addi a0, x0, 17
     ecall
